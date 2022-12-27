e9130d0e7c687d9dd494a75b38af2368854b879b
namiqmamedov <namigmm@code.edu.az>
2022-12-28 02:04:30 +0400

    feat: Added new home content

--- script/nav-menu.js
+++ script/nav-menu.js
@@ -1,35 +1,17 @@
-$(document).ready(function(){
+$(function () {
 
-    $('#button-menu').click(function(){
-      if($('#button-menu').attr('class') == 'fa fa-bars' ){
-  
-        $('.navegacion').css({'width':'100%', 'background':'rgba(0,0,0,.5)'}); 
-        $('#button-menu').removeClass('fa fa-bars').addClass('fa fa-close');
-        $('.navegacion .menu').css({'left':'0px'});
-  
-      } else{
-  
-        $('.navegacion').css({'width':'0%', 'background':'rgba(0,0,0,.0)'});
-        $('#button-menu').removeClass('fa fa-close').addClass('fa fa-bars'); 
-        $('.navegacion .submenu').css({'left':'-320px'});
-        $('.navegacion .menu').css({'left':'-320px'}); 
-  
+
+  $(window).scroll(function () {
+
+    $(window).scroll(function () {
+      if ($(window).scrollTop() > 0) {
+        $(".main-header").addClass("sticky");
+      } else {
+        $(".main-header").removeClass("sticky");
       }
+
     });
-  
-    $('.navegacion .menu > .item-submenu a').click(function(){
-      
-      var position = <a href="https://www.jqueryscript.net/menu/">Menu</a> = $(this).parent().attr('menu');
-      console.log(positionMenu); 
-  
-      $('.item-submenu[menu='+positionMenu+'] .submenu').css({'left':'0px'}); 
-  
-    });
-  
-    $('.navegacion .submenu li.go-back').click(function(){
-  
-      $(this).parent().css({'left':'-320px'});
-  
-    });
-  
+
   });
+
+});
