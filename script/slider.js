41c0e5bb4e6f60ca272868bd2c2ddba7e96d179f
namiqmamedov <namigmm@code.edu.az>
2022-12-28 07:01:17 +0400

    refactor: Updated carousel for top deals

--- script/slider.js
+++ script/slider.js
@@ -3,6 +3,40 @@ $('#slider').slick({
     infinite: false,
     slidesToShow: 5,
     slidesToScroll: 3,
+   
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  });
+
+  
+$('#slider1').slick({
+    infinite: false,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+   
     responsive: [
       {
         breakpoint: 1024,
