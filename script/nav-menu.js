307d9450be6518af94daa54c3c08b4818cc2e17b
namiqmamedov <namigmm@code.edu.az>
2022-12-28 05:45:56 +0400

    feat: Added carousel for new product content

--- script/nav-menu.js
+++ script/nav-menu.js
@@ -1,17 +1,17 @@
-$(function () {
+// $(function () {
 
 
-  $(window).scroll(function () {
+//   $(window).scroll(function () {
 
-    $(window).scroll(function () {
-      if ($(window).scrollTop() > 0) {
-        $(".main-header").addClass("sticky");
-      } else {
-        $(".main-header").removeClass("sticky");
-      }
+//     $(window).scroll(function () {
+//       if ($(window).scrollTop() > 0) {
+//         $(".main-header").addClass("sticky");
+//       } else {
+//         $(".main-header").removeClass("sticky");
+//       }
 
-    });
+//     });
 
-  });
+//   });
 
-});
+// });
