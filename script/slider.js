a5e4a5faaa5a4821bcadc056583b68378e7d9633
namiqmamedov <namigmm@code.edu.az>
2022-12-29 01:14:15 +0400

    feat: Added featured categories for homepage

--- script/slider.js
+++ script/slider.js
@@ -63,3 +63,37 @@ $('#slider1').slick({
       }
     ]
   });
+
+
+    
+$('#slider2').slick({
+  infinite: false,
+  slidesToShow: 6,
+  slidesToScroll: 4,
+ 
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+  ]
+});
