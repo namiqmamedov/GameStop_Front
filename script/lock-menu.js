8eb0105a7b9ef8d422e5c1ad79f9f33f456e87ef
namiqmamedov <namigmm@code.edu.az>
2023-01-02 01:46:32 +0400

    refactor: Updated prev arrow and next arrow for slider

--- script/lock-menu.js
+++ script/lock-menu.js
@@ -1,13 +1,19 @@
 
+   if (window.innerWidth > 1000) {   
 
-const menu = $("#mMenu").click(function () {
-   if ($('body').addClass("lock-scroll")) {}
-})
+      const menu = $("#mMenu").click(function () {
+         if ($('body').addClass("lock-scroll")) {}
+      })
+      
+   }
+
+
 
 
 const menuover = $(".m-menu__overlay").click(function () {
    if ($('body').removeClass("lock-scroll")) {}
 })
+
 
 // $("#mMenu").click(function(){
 //    if ($('body').addClass("lock-scroll")){
