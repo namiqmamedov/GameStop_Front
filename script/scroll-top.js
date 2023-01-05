349e9022f0280942657f302da30b5d00042fc6e7
namiqmamedov <namigmm@code.edu.az>
2023-01-05 06:06:29 +0400

    refactor:Updated product cards for video game page

--- script/scroll-top.js
+++ script/scroll-top.js
@@ -1,35 +1,15 @@
-// const toTop = document.querySelector(".to-top");
+const toTop = document.querySelector(".to-top");
 
-// window.addEventListener("scroll", () => {
-//   if (window.pageYOffset > 235) {
-//     toTop.classList.add("active");
-//   } else {
-//     toTop.classList.remove("active");
-//   }
-// })
-
-
-
-// Get the button
-let mybutton = document.getElementsByClassName("myBtn");
-
-// When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {scrollFunction()};
-
-function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    mybutton.style.display = "block";
+window.addEventListener("scroll", () => {
+  if (window.pageYOffset > 235) {
+    toTop.classList.add("active");
   } else {
-    mybutton.style.display = "none";
+    toTop.classList.remove("active");
   }
-}
+})
 
 
-// When the user clicks on the button, scroll to the top of the document
-function topFunction() {
-  document.body.scrollTop = 0;
-  document.documentElement.scrollTop = 0;
-}
+
 
 
 
