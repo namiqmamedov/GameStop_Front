6724fbd7a350d7f3c2f9b31f03a3a98de2aacb28
namiqmamedov <namigmm@code.edu.az>
2023-01-18 06:19:48 +0400

    refactor: Updated input label for product detail

--- script/checkbox.js
+++ script/checkbox.js
@@ -1,22 +1,22 @@
 
-// $("input:checkbox").on('click', function() {
+$("input:checkbox").on('click', function() {
 
-//       // console.log('hello');
-//       var $box = $(this);
-//       if ($box.is(":checked")) {
+      // console.log('hello');
+      var $box = $(this);
+      if ($box.is(":checked")) {
      
-//       var group = "input:checkbox[name='" + $box.attr("name") + "']";
+      var group = "input:checkbox[name='" + $box.attr("name") + "']";
   
-//       $(group).prop("checked", false);
-//       $box.prop("checked", true);
-//     } 
+      $(group).prop("checked", false);
+      $box.prop("checked", true);
+    } 
     
     
-//     else {
-//       $box.prop("checked", false);
-//     }
+    else {
+      $box.prop("checked", false);
+    }
     
-//   });
+  });
 
 function clearRadioButtons(){
   var ele = document.querySelectorAll("input[type=radio],input[type=checkbox]");
@@ -24,6 +24,47 @@ function clearRadioButtons(){
       ele[i].checked = false;
    }
 }
+
+
+$(document).ready(function() {
+
+   var checkBox = $('.type');
+   var checkElem = $('input[type="checkbox"');
+   var deleteBtn = $('.choiceDelete');
+
+   $('.choiceBox').click(function() {
+       close();
+       checkDelete();
+   });
+
+  
+
+   checkItem();
+
+   checkElem.change(function(event) {
+       checkItem();
+       checkDelete();
+   });
+
+
+   function checkItem() {
+       var itemLabel = Array();
+       checkElem.each(function() {
+           if ($(this).prop('checked')) {
+               itemLabel.push($(this).next('label').text());
+           }
+       });
+       var itemLabelsStr = itemLabel.join();
+       if (itemLabel.length > 0) {
+           $('.platform-name').text(itemLabelsStr);
+       }
+   }
+
+
+
+});
+
+
 
 
 
