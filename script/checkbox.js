4d305910df71875e47ed9784c720b090bba62b39
namiqmamedov <namigmm@code.edu.az>
2023-01-18 07:01:29 +0400

    feat: Added new input label for product detail

--- script/checkbox.js
+++ script/checkbox.js
@@ -1,70 +1,70 @@
+$("input:checkbox").on('click', function () {
 
-$("input:checkbox").on('click', function() {
+   // console.log('hello');
+   var $box = $(this);
+   if ($box.is(":checked")) {
 
-      // console.log('hello');
-      var $box = $(this);
-      if ($box.is(":checked")) {
-     
       var group = "input:checkbox[name='" + $box.attr("name") + "']";
-  
+
       $(group).prop("checked", false);
       $box.prop("checked", true);
-    } 
-    
-    
-    else {
+   } else {
       $box.prop("checked", false);
-    }
-    
-  });
+   }
 
-function clearRadioButtons(){
-  var ele = document.querySelectorAll("input[type=radio],input[type=checkbox]");
-   for(var i=0;i<ele.length;i++){
+});
+
+function clearRadioButtons() {
+   var ele = document.querySelectorAll("input[type=radio],input[type=checkbox]");
+   for (var i = 0; i < ele.length; i++) {
       ele[i].checked = false;
    }
 }
 
 
-$(document).ready(function() {
+$(document).ready(function () {
 
-   var checkBox = $('.type');
-   var checkElem = $('input[type="checkbox"');
-   var deleteBtn = $('.choiceDelete');
+   var checkElem = $('.platform-box input[type="checkbox"');
+   var checkElem1 = $('.condition-box input[type="checkbox"');
 
-   $('.choiceBox').click(function() {
-       close();
-       checkDelete();
-   });
-
-  
 
    checkItem();
 
-   checkElem.change(function(event) {
-       checkItem();
-       checkDelete();
+   checkElem.change(function (event) {
+      checkItem();
+      checkDelete();
    });
 
 
+   
+   checkElem1.change(function (event) {
+      checkItem();
+      checkDelete();
+   });
+
+
+
    function checkItem() {
-       var itemLabel = Array();
-       checkElem.each(function() {
-           if ($(this).prop('checked')) {
-               itemLabel.push($(this).next('label').text());
-           }
-       });
-       var itemLabelsStr = itemLabel.join();
-       if (itemLabel.length > 0) {
-           $('.platform-name').text(itemLabelsStr);
-       }
+      var itemLabel = Array();
+      var itemLabel1 = Array();
+      checkElem.each(function () {
+         if ($(this).prop('checked')) {
+            itemLabel.push($(this).next('label').text());
+         }
+      });
+      checkElem1.each(function () {
+         if ($(this).prop('checked')) {
+            itemLabel1.push($(this).next('label').text());
+         }
+      });
+      var itemLabelsStr = itemLabel.join();
+      var itemLabelsStr1 = itemLabel1.join();
+      if (itemLabel.length > 0) {
+         $('.platform-name').text(itemLabelsStr);
+      } 
+      if (itemLabel1.length > 0) {
+         $('.condition-name').text(itemLabelsStr1);
+      } 
    }
 
-
-
 });
-
-
-
-
-
